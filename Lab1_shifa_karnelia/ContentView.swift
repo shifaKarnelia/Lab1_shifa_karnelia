c1b62400397eee8d5233b838847d64fded5e70b2
shifaKarnelia <shifakarnelia@gmail.com>
2026-02-28 13:35:04 -0500

    Initial Commit

---
+++ Lab1_shifa_karnelia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_shifa_karnelia
+//
+//  Created by Shifa Karnelia on 2026-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_shifa_karnelia/Lab1_shifa_karneliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_shifa_karneliaApp.swift
+//  Lab1_shifa_karnelia
+//
+//  Created by Shifa Karnelia on 2026-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_shifa_karneliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
