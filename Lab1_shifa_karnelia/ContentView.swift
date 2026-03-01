34f004718c2a530c4fab06dd5105604519d26aed
shifaKarnelia <shifakarnelia@gmail.com>
2026-02-28 19:36:36 -0500

    show the check mark/cross symbole base on user answer

--- Lab1_shifa_karnelia/ContentView.swift
+++ Lab1_shifa_karnelia/ContentView.swift
@@ -14,62 +14,74 @@ struct ContentView: View {
     @State private var userAnsweredThisRound: Bool = false
     
     enum Feedback {
-            case none
-            case correct
-            case wrong
-        }
+        case none
+        case correct
+        case wrong
+    }
     var body: some View {
         
         //print number
         Text(" \(currentNumber)")
             .font(.system(size: 64, weight: .semibold, design: .serif))
             .foregroundColor(.teal)
-
+        
         //prime , not prime label
         VStack(spacing: 18) {
-        tappableChoice(title: "Prime", userChoicePrime: true)
-        tappableChoice(title: "not Prime", userChoicePrime: false)
+            tappableChoice(title: "Prime", userChoicePrime: true)
+            tappableChoice(title: "not Prime", userChoicePrime: false)
+        }
+        
+        Group {
+            switch feedback {
+                case .none:
+                    EmptyView()
+                      .frame(height: 80)
+                case .correct:
+                    Image(systemName: "checkmark.circle.fill")
+                       .font(.system(size: 72))
+                        .foregroundColor(.green)
+                        .frame(height: 80)
+                case .wrong:
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.system(size: 72))
+                        .foregroundColor(.red)
+                        .frame(height: 80)
+                           }
                        }
-       
-
-        EmptyView().frame(height: 80)
-        Image(systemName: "checkmark.circle.fill")
-            .font(.system(size: 72))
-            .foregroundColor(.green)
-            .frame(height: 80)
-                            
-        Image(systemName: "xmark.circle.fill")
-            .font(.system(size: 72))
-            .foregroundColor(.red)
-            .frame(height: 80)
     }
     //function that
     //check if number is prime or not
     private func isPrime(_ n: Int) -> Bool {
-            if n < 2 { return false }
-            if n == 2 { return true }
-            if n % 2 == 0 { return false }
-
-            let limit = Int(Double(n).squareRoot())
-            if limit < 3 { return true }
-
-            var i = 3
-            while i <= limit {
-                if n % i == 0 { return false }
-                i += 2
-            }
-            return true
+        if n < 2 { return false }
+        if n == 2 { return true }
+        if n % 2 == 0 { return false }
+        
+        let limit = Int(Double(n).squareRoot())
+        if limit < 3 { return true }
+        
+        var i = 3
+        while i <= limit {
+            if n % i == 0 { return false }
+            i += 2
+        }
+        return true
     }
     private func tappableChoice(title: String, userChoicePrime: Bool) -> some View {
         Text(title)
-                    .font(.system(size: 34, weight: .regular, design: .serif))
-                    .foregroundColor(.teal)
-                    .padding(.vertical, 6)
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        print("  \(title) tapped")
-                    }
+            .font(.system(size: 34, weight: .regular, design: .serif))
+            .foregroundColor(.teal)
+            .padding(.vertical, 6)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                guard !userAnsweredThisRound else { return }
+                let actualPrime = isPrime(currentNumber)
+                let userIsCorrect = (userChoicePrime == actualPrime)
+                userAnsweredThisRound = true
+                feedback = userIsCorrect ? .correct : .wrong
+            }
     }
+    
+    
 }
 
 #Preview {
